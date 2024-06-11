749c2e752349ad3671820becbe944d6f1675e200
Marcela Andrade <marcelasrandrade@gmail.com>
2024-06-10 22:06:52 -0300

    Implementação da remoção dos vertices e da arestas.

---
+++ src/pacote/VerticeDistancia.java
@@ -0,0 +1,17 @@
+package pacote;
+
+public class VerticeDistancia<TIPO> {
+    private Vertice<TIPO> vertice;
+    private double distancia;
+    
+    public VerticeDistancia(Vertice<TIPO> vertice, double distancia){
+        this.vertice = vertice;
+        this.distancia = distancia;       
+    }
+    public Vertice<TIPO> getVertice(){
+        return vertice;
+    }
+    public double getDistancia(){
+        return distancia;
+    }
+}
